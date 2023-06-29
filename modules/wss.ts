ae83f0c5132401619892df8d63f41db9264acfbc
Battlekeeper <conwaywest@tech-center.com>
2023-06-29 15:27:14 -0400

    websocket testing

--- modules/wss.ts
+++ modules/wss.ts
@@ -1,14 +1,22 @@
 import { WebSocketServer } from 'ws';
+
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+function random(){
+    return Math.random();
+}
+
+
 export default function ExampleModule() {
     
     const wss = new WebSocketServer({ port: 2999});
     console.log('Initialized Websocket Server')
 
-    wss.on('connection', function connection(ws) {
+    wss.on('connection', async function connection(ws) {
         ws.on('message', function message(data) {
-            console.log('received: %s', data);
+            ws.send(random());
         });
-
-        ws.send('something');
     });
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <script setup>
- const route = useRouter();
+    import { ref } from "vue"
+
+    const route = useRouter();
+    const number = ref(0);
+    const temp = ref(0);
+
+    let socket = new WebSocket('ws://localhost:2999');
+
+    socket.addEventListener('open', () => {
+        socket.send("Start");
+        console.log('Connected to WebSocket server');
+    });
+
+    socket.addEventListener('message', (event) => {
+        var incoming = Number.parseFloat(event.data);
+        incoming = Math.floor(incoming * 1000000);
+        temp.value = incoming;
+        if (incoming > number.value)
+        number.value = incoming;
+        socket.send("Message");
+    });
+
+    socket.addEventListener('close', () => {
+        console.log('Disconnected from WebSocket server');
+    });
 </script>
 
 <template>
-    <p>This is some text</p>
+    <p>This is a random number: {{number}}</p>
+    <br>
+    <p>{{temp}}</p>
+
 </template>
